4a8fccccbf1ee760fbeeb077d28d17a99f4ba926
Daniel R Meneses <daniel,meneses@ironbit.com.mx>
2020-03-01 08:39:42 -0600

    Initial Commit

---
+++ ExamenMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExamenMVP
+//
+//  Created by Daniel Meneses on 01/03/20.
+//  Copyright © 2020 Daniel Meneses. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
